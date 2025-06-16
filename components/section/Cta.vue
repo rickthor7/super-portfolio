7c3837e58f0fb32249839dee710d476f576a4863
rickthor7 <96973185+rickthor7@users.noreply.github.com>
2025-06-16 23:42:09 +0700

    Update Cta.vue

--- components/section/Cta.vue
+++ components/section/Cta.vue
@@ -36,7 +36,7 @@
         </div>
         </div>
 
-        <p class="text-sm text-white/30 text-center mt-8">&copy; Copyright <NuxtLink to="https://rauliqbal.my.id" target="_blank">Rauliqbal</NuxtLink> 2024</p>
+        <p class="text-sm text-white/30 text-center mt-8">&copy; Copyright <NuxtLink to="https://rauliqbal.my.id" target="_blank">Rickthor7</NuxtLink> 2025</p>
 
         <div
           class="absolute  bottom-0 w-60 h-[1px] z-10 bg-gradient-to-r from-[#0b50e5]/0 via-[#0b50e5] to-[#0b50e5]/0 ADAWDDWDAWDAWD"
